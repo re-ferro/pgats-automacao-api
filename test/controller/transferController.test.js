6744e60198d62cc217d128df47186fca1408a465
re-ferro <re.ferro123@gmail.com>
2025-09-04 20:46:08 -0300

    Inserindo respostas

--- test/controller/transferController.test.js
+++ test/controller/transferController.test.js
@@ -39,9 +39,45 @@ describe('Transfer Controller', () => {
 
             expect(resposta.status).to.equal(400);
             expect(resposta.body).to.have.property('error', 'Usuário remetente ou destinatário não encontrado.');
+            })
 
-            sinon.restore();
+            it('Usando Mocks: Quando informo valores válidos eu tenho sucesso com 201 CREATED', async () => {
+            // Mocar apenas a função transfer do Service
+            const transferServiceMock = sinon.stub(transferService, 'transferValue');
+            transferServiceMock.returns({ 
+                transfer: {
+                    from: "re", 
+                    to: "fe", 
+                    value: 100, 
+                    date: new Date().toISOString() 
+                }
             });
+
+            const resposta = await request(app)
+                .post('/transfer')
+                .send({
+                    from: "re",
+                    to: "fe",
+                    value: 100
+                });
+            
+            expect(resposta.status).to.equal(201);
+            
+            // Validação com um Fixture
+            const respostaEsperada = require('../fixture/respostas/quandoInformoValoresValidosEuTenhoSucessoCom201Created.json')
+            delete resposta.body.date;
+            delete respostaEsperada.date; 
+            expect(resposta.body).to.deep.equal(respostaEsperada);
+
+            // Um expect para comparar a Resposta.body com a String contida no arquivo
+            // expect(resposta.body).to.have.property('from', 'julio');
+            // expect(resposta.body).to.have.property('to', 'priscila');
+            // expect(resposta.body).to.have.property('value', 100);
         });
+
+        afterEach(() => {
+            // Reseto o Mock
+            sinon.restore();
+        })
     });
-    
+});
